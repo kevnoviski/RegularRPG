f12e53f2357c2536201b9d6a41635e75470213ed
Oem <Oem@OEM-PC>
2021-02-20 21:20:14 -0300

    minor changes.

--- src/dao/JumpStartDB.java
+++ src/dao/JumpStartDB.java
@@ -14,39 +14,23 @@ public class JumpStartDB {
 	
 	public void Create() {
 		Crud crud = new Crud();
-		//crud.Create("create table central.teste (id int)");
-		
-		/*
-		List<String> parms = new ArrayList<String>();
-		parms.add("1");
-		
-		crud.Insert("insert into central.teste values(?)",parms );
-		
-		crud.DeleteFrom("central.teste", "id=?", parms);
-		*/
-           
+          
 		DataTable res = crud.Select("select st.tablename  from sys.systables st "
 				+ "LEFT OUTER join sys.sysschemas ss on "
 				+ "(st.schemaid = ss.schemaid) where ss.schemaname ='REGRPG'\r\n");
-                res.PrintTable();
+                
 		if(res.getRowCount() == 0) {
                     try {
+                        System.out.print("The required tables were not found, creating new ones..");
                         String rootFolder=new File("").getAbsolutePath().toString();
                         Path fileName = Path.of(rootFolder+"\\startupDB.sql");
                         String newTables = Files.readString(fileName);
                         crud.RunBatchCommand(newTables);
+                        System.out.print("done.");
                     } catch (IOException ex) {
                         Logger.getLogger(JumpStartDB.class.getName()).log(Level.SEVERE, null, ex);
                     }
 		}
-                System.out.print("----");
-		res = crud.Select("select st.tablename  from sys.systables st "
-				+ "LEFT OUTER join sys.sysschemas ss on "
-				+ "(st.schemaid = ss.schemaid) where ss.schemaname ='REGRPG'\r\n");
-                res.PrintTable();
-		/*select st.tablename  from sys.systables st LEFT OUTER join sys.sysschemas ss on (st.schemaid = ss.schemaid) where ss.schemaname ='APP'
-*/
-		
 	}
 	
 }
